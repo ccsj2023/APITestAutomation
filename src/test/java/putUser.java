ee7e0346af63a32a90e155fbe0fc67298558604f
ccsj2023 <149729635+ccsj2023@users.noreply.github.com>
2025-09-13 15:15:46 -0400

    Update putUser.java
    adding java doc to putuser class

--- src/test/java/putUser.java
+++ src/test/java/putUser.java
@@ -9,8 +9,27 @@ import java.io.IOException;
 import static helper.BaseTestHelper.fileInputStreamMethod;
 import static io.restassured.RestAssured.given;
 import static org.testng.Assert.assertEquals;
-
+/**
+ * This class contains TestNG tests for validating PUT requests to the reqres.in API.
+ * It demonstrates different ways of providing the request body:
+ * <ul>
+ * <li>Using a JSON string directly.</li>
+ * <li>Reading a JSON payload from an external file.</li>
+ * <li>Using a Plain Old Java Object (POJO).</li>
+ * </ul>
+ * <p>
+ * The tests verify that the API returns a successful status code (200 OK) for each PUT operation.
+ * The class uses RestAssured for making the HTTP requests and TestNG for test execution and assertions.
+ * </p>
 public class putUser {
+   /**
+     * Validates a PUT request where the request body is provided as a JSON string.
+     * <p>
+     * It sends a PUT request to update user with ID 2 on the reqres.in API.
+     * The request body is a hardcoded JSON string: `{"name":"morpheus","job":"leader"}`.
+     * It asserts that the response status code is 200 (SUCCESS).
+     * </p>
+     */
     @Test
     public void validatePutWithString(){
 
@@ -24,7 +43,16 @@ public class putUser {
         System.out.println("validatePutWithString executed successfully");
         System.out.println(response.getBody().asString());
     }
-
+   /**
+     * Validates a PUT request where the request body is read from an external JSON file.
+     * <p>
+     * It uses `IOUtils.toString()` to convert the content of `putRequestBody.json` file
+     * into a string which is then used as the request body.
+     * It asserts that the response status code is 200 (SUCCESS).
+     * </p>
+     *
+     * @throws IOException if an I/O error occurs while reading the file.
+     */
     @Test
     public void validatePutWithJsonFile()throws IOException {
 
@@ -39,6 +67,14 @@ public class putUser {
         System.out.println(response.getBody().asString());
     }
 
+    /**
+     * Validates a PUT request where the request body is created using a POJO (Plain Old Java Object).
+     * <p>
+     * It creates an instance of `postRequestBody` POJO, sets the `name` and `job` fields,
+     * and uses this object as the request body. RestAssured automatically serializes the POJO to JSON.
+     * It asserts that the response status code is 200 (SUCCESS).
+     * </p>
+     */
     @Test
     public void validatePutWithPojo(){
         postRequestBody putRequest = new postRequestBody();

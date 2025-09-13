b2ed4e970caa4cd0b0b2a0f95f1fba9fd1f6b204
ccsj2023 <149729635+ccsj2023@users.noreply.github.com>
2025-09-13 15:41:21 -0400

    Update jsonSchemaValidation.java

--- src/test/java/jsonSchemaValidation.java
+++ src/test/java/jsonSchemaValidation.java
@@ -9,10 +9,8 @@ import static io.restassured.RestAssured.given;
  * This class contains a TestNG test for validating the JSON schema of an API response.
  * It demonstrates how to use RestAssured's {@link JsonSchemaValidator} to ensure that
  * the structure of the JSON response body conforms to a predefined schema file.
- * <p>
  * The test fetches a list of users from the reqres.in API and validates the response
  * against a schema file located at `resources/Schema/ExpectedSchema.json`.
- * </p>
  *
  * @author Your Carolina Steadham 
  */   
@@ -20,16 +18,12 @@ public class jsonSchemaValidation {
 
     /**
      * Validates the JSON schema of the response from the users API endpoint.
-     * <p>
      * This test performs the following steps:
-     * <ol>
-     * <li>Loads the expected JSON schema from a file.</li>
-     * <li>Sends a GET request to `https://reqres.in/api/users?page=234`.</li>
-     * <li>Asserts that the response status code is 200.</li>
-     * <li>Validates the response body against the loaded JSON schema using
+     * Loads the expected JSON schema from a file.</li>
+     * Sends a GET request to `https://reqres.in/api/users?page=234`.</li>
+     * Asserts that the response status code is 200.</li>
+     * Validates the response body against the loaded JSON schema using
      * {@link JsonSchemaValidator#matchesJsonSchema(File)}.</li>
-     * </ol>
-     * </p>
      */
     @Test
     public void jsonSchemaValidation() {

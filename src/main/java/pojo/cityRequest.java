c591d9b810614339d8f314034082abe00981af17
ccsj2023 <149729635+ccsj2023@users.noreply.github.com>
2025-09-17 19:38:34 -0400

    Update cityRequest.java

--- src/main/java/pojo/cityRequest.java
+++ src/main/java/pojo/cityRequest.java
@@ -1,5 +1,16 @@
 package pojo;
 
+/*
+ * This is a Plain Old Java Object (POJO) class representing a city request.
+ * It is used to model the request body for an API call, typically to add or update city data.
+ *
+ * This class encapsulates two fields: name and temperature, and provides
+ * standard getter and setter methods to access and modify these properties.
+ * Using a POJO helps with serialization and deserialization of JSON or XML data
+ * in API automation testing.
+ *
+ * @author Carolina Steadham
+ */
 public class cityRequest {
 
     private String name;
